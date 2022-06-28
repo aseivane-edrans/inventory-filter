f8978a872a9bdf83dbb9a17629e40843324cae4b
tu nombre <tu@correo.com>
2022-06-28 14:27:57 -0300

    prettyTables

--- filter.py
+++ filter.py
@@ -1,12 +1,12 @@
 import argparse
 import csv
-#from prettytable import PrettyTable
-'''
+from prettytable import PrettyTable
+
 def printTable(list):
     table = PrettyTable()
     table.field_names = ["id", "task", "environment", "location", "team"]
     table.add_rows( list )
-    print (table)'''
+    print (table)
     
 def openfile(file):
     try:
@@ -22,11 +22,11 @@ class argFilter(argparse.ArgumentParser):
         argparse.ArgumentParser.__init__(self,**kwarg)
     def parse_arguments(self):
         self.add_argument('-f', type=str, dest='file')
-        self.add_argument('-i', type=str, nargs='+', dest='id')
-        self.add_argument('-k', type=str, nargs='+', dest='task')
-        self.add_argument('-e', type=str, nargs='+', dest='env')
-        self.add_argument('-l', type=str, nargs='+', dest='loc')
-        self.add_argument('-t', type=str, nargs='+', dest='team')
+        self.add_argument('-i', type=str, dest='id')
+        self.add_argument('-k', type=str, dest='task')
+        self.add_argument('-e', type=str, dest='env')
+        self.add_argument('-l', type=str, dest='loc')
+        self.add_argument('-t', type=str, dest='team')
         
         self.args = self.parse_args()
 
@@ -43,19 +43,19 @@ class argFilter(argparse.ArgumentParser):
         # append every argument list
         self.arg_list = []
         if self.args.id:
-            self.arg_list = self.arg_list + self.args.id
+            self.arg_list.append(self.args.id)
         
         if self.args.task:
-            self.arg_list = self.arg_list + self.args.task
+            self.arg_list.append(self.args.task)
 
         if self.args.env:
-            self.arg_list = self.arg_list + self.args.env
+            self.arg_list.append(self.args.env)
 
         if self.args.loc:
-            self.arg_list = self.arg_list + self.args.loc
+            self.arg_list.append(self.args.loc)
 
         if self.args.team:
-            self.arg_list = self.arg_list + self.args.team
+            self.arg_list.append(self.args.team)
         
         self.arg_set = set(self.arg_list)
     
@@ -71,16 +71,5 @@ lista = openfile(parser.args.file)
 
 filtered_list = list( parser.list_filter(lista) )
 
-for line in filtered_list:
-    print(*line)
-
-'''
-for row in list:
-    if args.id:
-        if args.id not in row:
-
-
-
-if(list):
-    printTable(list)
-    '''
+if(filtered_list):
+    printTable(filtered_list)
